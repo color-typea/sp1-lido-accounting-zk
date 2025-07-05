81eec03a1c2019a8d9327a6837c43c390b8dc0ea
color-typea <colorcode6a@gmail.com>
2025-07-05 16:21:57 +0800

    Improve message for beacon block hash mismatch

--- contracts/src/Sp1LidoAccountingReportContract.sol
+++ contracts/src/Sp1LidoAccountingReportContract.sol
@@ -82,6 +82,8 @@ contract Sp1LidoAccountingReportContract is SecondOpinionOracle, Ownable, Pausab
     /// @dev Verification failed
     error VerificationError(string error_message);
 
+    error BeaconBlockHashMismatch(bytes32 expected, bytes32 actual);
+
     /// @dev Caller have to be authorized to call pause
     error UnauthorizedPauseAccount();
 
@@ -265,7 +267,7 @@ contract Sp1LidoAccountingReportContract is SecondOpinionOracle, Ownable, Pausab
         // Check that passed beacon_block_hash matches the one observed on the blockchain for
         // the target slot
         bytes32 expected_block_hash = _findBeaconBlockHash(metadata.bc_slot);
-        require(metadata.beacon_block_hash == expected_block_hash, VerificationError("BeaconBlockHash mismatch"));
+        require(metadata.beacon_block_hash == expected_block_hash, BeaconBlockHashMismatch(expected_block_hash, metadata.beacon_block_hash));
 
         // Check that correct withdrawal credentials were used
         require(
